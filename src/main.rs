ba3aab2adaff50db9b7b53975e1972286206a769
ugonight <amihajime@yahoo.co.jp>
2023-01-07 17:46:59 +0900

    stage3 take4

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-use tokio::time::error::Elapsed;
-
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
